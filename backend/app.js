1c07bab4fa67d7e5b6c6892ee6b5ac20104bf5b2
AhmedAbdo1612 <102939327+AhmedAbdo1612@users.noreply.github.com>
2023-12-25 22:21:32 +0200

    header

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,14 @@ dotenv.config()
 app.use(bodyPrser.json())
 app.use(bodyPrser.urlencoded({extended:true}))
 app.use(cookieParser())
-
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
 app.use('/',express.static('uploads'))
 // app.use(fileUpload({useTempFiles:true}))
 app.use('/api/v2/user',userRouter)
