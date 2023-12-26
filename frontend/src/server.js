0ba5a4db79bb9240d4cac78a90c059c6dd200937
AhmedAbdo1612 <102939327+AhmedAbdo1612@users.noreply.github.com>
2023-12-26 13:49:17 +0200

    again

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server =      "https://e-commerce-rnkl.onrender.com/api/v2/"
+export const server =      "https://e-commerce-rnkl.onrender.com/api/v2"
 export const backend_url = "https://e-commerce-rnkl.onrender.com/"
